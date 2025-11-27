d3cbccf04e2807b1b89d3cdf1b208d22ed6c254e
Md Mohsin Alam <mohsinalam1141@gmail.com>
2025-11-27 15:01:39 +0600

    Home page added

--- JS/Login.js
+++ JS/Login.js
@@ -2,6 +2,15 @@
 document.getElementById('button-login').addEventListener('click',function(event){
     event.preventDefault();
     const phoneNumber=document.getElementById('phone-number').value;
-    
+    const pinNumber=document.getElementById('pin-number').value;
+    console.log(phoneNumber,pinNumber);
+    if (phoneNumber==='015' && pinNumber==='1234'){
+        console.log('You are logged in')
+        window.location.href="/JS/home.html"
+
+    }
+    else{
+        alert('Wrong Phone number or Pin Number')
+    }
 
 })
