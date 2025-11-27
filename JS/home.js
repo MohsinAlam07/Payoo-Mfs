5701b56cab7a3b83f01b0d47f9a3abb337ece5bd
Md Mohsin Alam <mohsinalam1141@gmail.com>
2025-11-27 15:38:36 +0600

    Pin Number and Money

--- JS/home.js
+++ JS/home.js
@@ -1,1 +1,9 @@
-console.log('home added');
+// console.log('home added');
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    event.preventDefault();
+    // console.log('click the add money button')
+    const addMoneyInput=document.getElementById('input-add-money').value;
+    // console.log(addMoneyInput)
+    const pinNumberInput=document.getElementById('input-pin-number').value;
+console.log(pinNumberInput)
+})
